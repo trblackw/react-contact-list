ac972e073993675961a20d152e6cbf4e766bdfc7
Tucker <trblackw@ncsu.edu>
2018-11-27 15:24:47 -0500

    copied files over from broken repo and installed necessary dependencies

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Contacts from "./components/Contacts";
+import AddContact from "./components/AddContact";
+import EditContact from "./components/EditContact";
+import ContactsContextProvider from "./components/ContactsContextProvider";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <ContactsContextProvider>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Contacts} />
+        <Route path="/add" component={AddContact} />
+        <Route path="/:id" component={EditContact} />
+      </Switch>
+    </Router>
+  </ContactsContextProvider>
+);
 
 export default App;
