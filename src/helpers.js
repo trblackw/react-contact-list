d426fa34efb602ac8f7d3698db517893d36d55bd
Tucker <trblackw@ncsu.edu>
2018-11-28 17:09:13 -0500

    modified formatter helper function to better format data received from random people api

--- src/helpers.js
+++ src/helpers.js
@@ -7,3 +7,26 @@ export const reducer = arr => {
   const { name, email, phone_number, image_url } = newState;
   return { name, email, phone_number, image_url };
 };
+
+export const formatter = arr => {
+  return arr.map(
+    ({ name, location, phone: phone_number, picture: image_url, email }) => ({
+      id: Math.round(Math.random() * 100000000),
+      name: `${name.first[0].toUpperCase()}${name.first
+        .split("")
+        .slice(1)
+        .join("")} ${name.last[0].toUpperCase()}${name.last
+        .split("")
+        .slice(1)
+        .join("")}`,
+      location: `${location.city[0].toUpperCase()}${location.city
+        .split("")
+        .slice(1).join('')}, ${location.state[0].toUpperCase()}${location.state
+        .split("")
+        .slice(1).join('')}`,
+      phone_number: phone_number.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3"),
+      image_url: image_url.large,
+      email
+    })
+  );
+};
