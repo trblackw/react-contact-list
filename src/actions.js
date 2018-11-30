f7d9073758946614c755f96970faf238d9ab4b72
Tucker <trblackw@ncsu.edu>
2018-11-30 10:31:57 -0500

    Implemented add, edit and remove actions. Add and edit appear to be working but I need to find a way around having the api be fetched on every mounting of Contacts component

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,8 @@
 import { formatter } from "./helpers";
 export const FETCH_CONTACTS = "FETCH_CONTACTS";
 export const DELETE_CONTACT = "DELETE_CONTACT";
+export const EDIT_CONTACT = "EDIT_CONTACT";
+export const ADD_CONTACT = "ADD_CONTACT";
 
 export const fetchContacts = () => {
   return async dispatch => {
@@ -17,6 +19,30 @@ export const fetchContacts = () => {
     } catch (error) {
       console.error(`ERROR IN ACTIONS ${error}`);
     }
+  };
+};
+
+export const addContact = (contacts, contactToAdd) => ({
+  type: ADD_CONTACT,
+  contacts: contacts.concat(contactToAdd)
+});
+
+export const editContact = (contacts, id) => {
+  return dispatch => {
+    const contactToEdit = contacts.find(contact => contact.id === Number(id));
+    const updatedContacts = contacts.map(contact => {
+      if (contact.id === contactToEdit.id) {
+        contact = contactToEdit;
+        return contact;
+      } else {
+        return contact;
+      }
+    });
+
+    return dispatch({
+      type: EDIT_CONTACT,
+      contacts: updatedContacts
+    });
   };
 };
 
--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,9 @@
-import { FETCH_CONTACTS, DELETE_CONTACT } from './actions';
+import {
+  FETCH_CONTACTS,
+  DELETE_CONTACT,
+  EDIT_CONTACT,
+  ADD_CONTACT
+} from "./actions";
 
 const initialState = {
   contacts: []
@@ -8,6 +13,21 @@ export default function reducer(state = initialState, action) {
   const { type, contacts } = action;
   switch (type) {
     case FETCH_CONTACTS:
+      return {
+        ...state,
+        contacts
+      };
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts
+      };
+    case EDIT_CONTACT:
       return {
         ...state,
         contacts
